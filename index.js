b4f0801ced7d8ec6e2d08e8e3ebff64ae7d2574f
Peter Nied <peternied@hotmail.com>
2023-07-26 11:30:49 -0500

    Add way to specific sections to be removed and way to check for checklist items.
    Note; needs updated dist

--- index.js
+++ index.js
@@ -3,16 +3,59 @@ const github = require("@actions/github");
 
 function run() {
   const pullRequest = github.context.payload.pull_request;
+  const replacedTextSections = core.getInput('replaced-text-sections', { required: true }).split('\n');
+  const checklistItems = core.getInput('checklist-items', { required: true }).split('\n');
 
-  if(pullRequest) {
+
+  if (pullRequest) {
     const body = pullRequest.body;
 
-    if (!body) {
-      core.setFailed(
-        "The pull request description is empty. Please add a description."
-      );
+    const errorMessages = checkForErrors(body, replacedTextSections, checklistItems);
+    if (errorMessages.length != 0) {
+      core.setFailed(`Found ${errorMessages.length} errors:\n` + errorMessages.map(msg => '   - ' + msg).join('\n'));
     }
   }
+}
+
+function checkForErrors(body, replacedTextSections, checklistItems) {
+  const errorMessages = [];
+
+  if (replacedTextSections) {
+    for (let i = 0; i < replacedTextSections.length; i++) {
+      let errorMessage = textShouldBeReplaced(replacedTextSections[i]);
+      if (errorMessage) {
+        errorMessages.push(errorMessage);
+      }
+    }
+  }
+
+  if (checklistItems) {
+    for (let i = 0; i < checklistItems.length; i++) {
+      let errorMessage = checkForCheckListItem(checklistItems[i]);
+      if (errorMessage) {
+        errorMessages.push(errorMessage);
+      }
+    }
+  }
+
+  if (!body) {
+    errorMessages.push("The pull request description is empty. Please add a description.");
+  }
+  return errorMessages;
+}
+
+function textShouldBeReplaced(body, item) {
+  if (body && body.contains(item)) {
+    return `Found instructive test that should be replaced.  \`${item}\``;
+  }
+  return null;
+}
+
+function checkForCheckListItem(body, item) {
+  if (body && body.contains(item)) {
+    return `Found check list item that should be checked \`[X]\` or the item should be 'struck out' with \`~\` characters on both sides of the text. \`${item}\``
+  }
+  return null;
 }
 
 run();
